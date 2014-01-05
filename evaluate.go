3f711e73fb84144920477e7213900b0ef27a97cc
jiyinyiyong <jiyinyiyong@gmail.com>
2014-01-06 00:40:32 +0800

    rename codeLine to xs

--- evaluate.go
+++ evaluate.go
@@ -13,39 +13,41 @@ type Object struct {
 type Env map[string]Object
 
 // Evaluate read expressions and return a single result
-func Evaluate(env *Env, codeLine cirru.List) (ret Object) {
-  // debugPrint(codeLine, *env)
-  if len(codeLine) == 0 {
-    emptyArray := Object{"list", codeLine}
+func Evaluate(env *Env, xs cirru.List) (ret Object) {
+  // debugPrint(xs, *env)
+  if len(xs) == 0 {
+    emptyArray := Object{"list", xs}
     return emptyArray
   }
 
-  head := codeLine[0]
+  head := xs[0]
 
   if token, ok := head.(cirru.Token); ok {
     // debugPrint(token.Text)
     switch token.Text {
-    case "--": ret = cirruComment (env, codeLine[1:])
-    case "array": ret = cirruArray (env, codeLine[1:])
-    case "block": ret = cirruBlock(env, codeLine[1:])
-    case "bool": ret = cirruBool(env, codeLine[1:])
-    case "call": ret = cirruCall(env, codeLine[1:])
-    case "child": ret = cirruChild(env, codeLine[1:])
-    case "code": ret = cirruCode(env, codeLine[1:])
-    case "eval": ret = cirruEval(env, codeLine[1:])
-    case "float": ret = cirruFloat(env, codeLine[1:])
-    case "get": ret = cirruGet(env, codeLine[1:])
-    case "int": ret = cirruInt(env, codeLine[1:])
-    case "map": ret = cirruMap(env, codeLine[1:])
-    case "print": ret = cirruPrint(env, codeLine[1:])
-    case "regexp": ret = cirruRegexp(env, codeLine[1:])
-    case "require": ret = cirruRequire(env, codeLine[1:])
-    case "self": ret = cirruSelf(env, codeLine[1:])
-    case "set": ret = cirruSet(env, codeLine[1:])
-    case "string": ret = cirruString(env, codeLine[1:])
-    case "type": ret = cirruType(env, codeLine[1:])
-    case "under": ret = cirruUnder(env, codeLine[1:])
-    default: stop(token.Text, "not found")
+    case "--": ret = cirruComment (env, xs[1:])
+    case "array": ret = cirruArray (env, xs[1:])
+    case "block": ret = cirruBlock(env, xs[1:])
+    case "bool": ret = cirruBool(env, xs[1:])
+    case "call": ret = cirruCall(env, xs[1:])
+    case "child": ret = cirruChild(env, xs[1:])
+    case "code": ret = cirruCode(env, xs[1:])
+    case "eval": ret = cirruEval(env, xs[1:])
+    case "float": ret = cirruFloat(env, xs[1:])
+    case "get": ret = cirruGet(env, xs[1:])
+    case "int": ret = cirruInt(env, xs[1:])
+    case "map": ret = cirruMap(env, xs[1:])
+    case "print": ret = cirruPrint(env, xs[1:])
+    case "regexp": ret = cirruRegexp(env, xs[1:])
+    case "require": ret = cirruRequire(env, xs[1:])
+    case "self": ret = cirruSelf(env, xs[1:])
+    case "set": ret = cirruSet(env, xs[1:])
+    case "string": ret = cirruString(env, xs[1:])
+    case "type": ret = cirruType(env, xs[1:])
+    case "under": ret = cirruUnder(env, xs[1:])
+    default:
+      stop(token.Text, "not found")
+      ret = userCall(env, xs)
     }
     return
   }
@@ -53,5 +55,9 @@ func Evaluate(env *Env, codeLine cirru.List) (ret Object) {
     debugPrint(headExpression)
     return
   }
+  return
+}
+
+func userCall(env *Env, xs cirru.List) (ret Object) {
   return
 }
